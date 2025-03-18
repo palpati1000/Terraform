33225c5a77090dab866f4386e35194f439d53a19
palpati1000 <pravinalpati1000@Gmail.com>
2025-03-18 18:50:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 4.0"
+        }
+    }
+    backend "s3" {
+        bucket = "palpati1000" 
+        key    = "terraform.tfstate"
+        region = "ap-south-1" 
+        encrypt = true 
+  }
+}
+
 provider "aws" {
 region = "ap-south-1"
 }
