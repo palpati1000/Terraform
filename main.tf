b5165957c9087f88a5ac480460671a218268cea3
palpati1000 <pravinalpati1000@Gmail.com>
2025-03-18 19:22:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ region = "ap-south-1"
 
 resource "aws_key_pair" "t_key" {
     key_name   = "first-key-pair"
-    public_key = file("aws_terraform_key.pub")
+    public_key = file("aws_terraform_key")
     }
 
 resource "aws_vpc" "t_vpc" {
